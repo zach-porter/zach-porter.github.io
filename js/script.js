96df100b1d21ca287754207d47812c8239102e6a
Zachary Porter <75802402+zach-porter@users.noreply.github.com>
2024-08-21 14:27:31 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,24 +16,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Fade-in effect on scroll
     const faders = document.querySelectorAll('.fade-in');
+    const sliders = document.querySelectorAll('.slide-in');
 
     const appearOptions = {
         threshold: 0.5,
         rootMargin: "0px 0px -100px 0px"
     };
 
-    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
         entries.forEach(entry => {
             if (!entry.isIntersecting) {
                 return;
             } else {
                 entry.target.classList.add('show');
-                appearOnScroll.unobserve(entry.target); // Optional: Unobserve after it has appeared
+                observer.unobserve(entry.target); // Unobserve after it has appeared
             }
         });
     }, appearOptions);
 
     faders.forEach(fader => {
         appearOnScroll.observe(fader);
+    });
+
+    sliders.forEach(slider => {
+        appearOnScroll.observe(slider);
     });
 });
