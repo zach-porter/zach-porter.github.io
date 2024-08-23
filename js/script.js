6be652a18b0a3f0c7695afc9f4666176e3017063
Zachary Porter <75802402+zach-porter@users.noreply.github.com>
2024-08-22 22:40:36 -0700

    update

--- js/script.js
+++ js/script.js
@@ -69,3 +69,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var collapsibles = document.querySelectorAll('.collapsible');
+
+    collapsibles.forEach(function(collapsible) {
+        collapsible.addEventListener('click', function() {
+            this.classList.toggle('active');
+            var content = this.nextElementSibling;
+
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
